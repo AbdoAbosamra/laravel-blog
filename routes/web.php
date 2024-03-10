162e5716bc84d09f8f0bb237343e8befd696ad9b
Abdelrhman <abdo.abosamra80@gmail.com>
2024-03-10 03:23:52 +0200

    Add More Features

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/posts' , [PostController::class , 'index'])->name('posts.index');
+Route::get('posts/{post}' , [PostController::class ,'show'])->name('posts.show');
+
+// 1 - define a new route so the user can access it through  browser
+// 2 - define controller that renders a view
+// 3 - define view that contains list of posts
+// 4 - remove any static html data from the view
+// 5 - Handel Routeing and controller name convention
+
+
+// important : to hint to any Route by Route name route('route name')
